8da5448bab552541db5b1153f260775eebd298b9
adnanhd <adnanharundogan@gmail.com>
2024-08-28 16:24:37 +0300

    Fix unknonw type bug for python<=3.9 i.e. type1 | type2 -> Union[type1, type2]

--- callpyback/basics/scheduling.py
+++ callpyback/basics/scheduling.py
@@ -3,10 +3,17 @@ import inspect
 from copy import deepcopy
 from functools import partial
 from abc import ABC, abstractmethod
-from typing import Callable, List, Any, Dict
+from typing import Callable, List, Any, Dict, Tuple, Optional, Type, Union
+from types import FrameType
+
+ProfileFunction = Callable[[FrameType, str, Any], Any]
+CallbackFunction = Callable[..., Any]
+ExceptionTuple = Union[Tuple[Type[BaseException], ...], Type[BaseException]]
 
 
-def get_callback_kwargs(callback: Callable, kwargs: Dict[str, Any]) -> Dict[str, Any]:
+def get_callback_kwargs(
+    callback: CallbackFunction, kwargs: Dict[str, Any]
+) -> Dict[str, Any]:
     """Generates kwargs for the given callback function.
 
     Only kwargs specified in the callback signature will be passed to the callback.
@@ -27,10 +34,19 @@ def get_callback_kwargs(callback: Callable, kwargs: Dict[str, Any]) -> Dict[str,
 
 
 class Policy(ABC):
-    def __init__(self):
+    exceptions: Optional[ExceptionTuple]
+    tracer: Optional[ProfileFunction]
+
+    def __init__(
+        self,
+        exceptions: Optional[ExceptionTuple] = None,
+        tracer: Optional[ProfileFunction] = None,
+    ):
         self._names = list()
         self._localvars = dict()
         self._returnvars = dict()
+        self.exceptions = exceptions
+        self.tracer = tracer
 
     # def tracer(self, frame, event, arg):
     #     if event == "return":  # and frame.f_code.co_name in self._names:
@@ -38,79 +54,135 @@ class Policy(ABC):
     #         self._returnvars[frame.f_code.co_name] = deepcopy(arg)
 
     @abstractmethod
-    def prepare_callbacks(self, callbacks: List[Callable], **kwargs: Any) -> None:
+    def prepare_callbacks(
+        self, callbacks: List[CallbackFunction], **kwargs: Any
+    ) -> None:
         """Prepare the callbacks according to the policy."""
 
-    @abstractmethod
     def start(self) -> None:
+        if self.exceptions is not None:
+            try:
+                self._set_trace_profiler(self.tracer)
+                self._start()
+            except self.exceptions as e:
+                self._set_trace_profiler(None)
+                raise e
+        else:
+            self._set_trace_profiler(self.tracer)
+            self._start()
+
+    def join(self) -> None:
+        self._join()
+        self._set_trace_profiler(None)
+
+    @abstractmethod
+    def _start(self) -> None:
         """Start the execution of prepared callbacks."""
 
     @abstractmethod
-    def join(self) -> None:
+    def _set_trace_profiler(self, tracer: Union[ProfileFunction, None]) -> None:
+        """Set the tracer profile for reduction of callback-local parameters."""
+
+    @abstractmethod
+    def _join(self) -> None:
         """Wait for all the started callbacks to complete."""
 
 
 class SinglePolicy(Policy):
-    def __init__(self, tracer=None):
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
         self.callbacks = []
-        self.tracer = tracer
 
-    def prepare_callbacks(self, callbacks: List[Callable], **kwargs: Any):
+    def prepare_callbacks(self, callbacks: List[CallbackFunction], **kwargs: Any):
         """Prepares the callbacks for execution by storing them."""
         self.callbacks = [
             (callback, get_callback_kwargs(callback, kwargs)) for callback in callbacks
         ]
 
-    def start(self) -> None:
+    def _start(self) -> None:
         """Executes the callbacks sequentially."""
-        sys.setprofile(self.tracer)
         for callback, kwargs in self.callbacks:
             callback(**kwargs)
-        sys.setprofile(None)
 
-    def join(self) -> None:
+    def _join(self) -> None:
         """No-op for SinglePolicy since everything runs sequentially."""
+
+    def _set_trace_profiler(self, tracer: Union[ProfileFunction, None]) -> None:
+        sys.setprofile(tracer)
 
 
 import threading
 
 
-class ThreadingPolicy(Policy):
-    def __init__(self, tracer=None):
-        self.threads = []
-        self.tracer = tracer
+class ExceptionThread(threading.Thread):
+    def __init__(self, exceptions: Optional[ExceptionTuple] = None, **kwds):
+        assert exceptions is None or (
+            isinstance(exceptions, tuple)
+            and all(map(BaseException.__subclasscheck__, exceptions))
+            or issubclass(exceptions, BaseException)
+        ), "exceptions must be a tuple of exceptions"
+        self._exceptions = exceptions
+        super().__init__(**kwds)
 
-    def prepare_callbacks(self, callbacks: List[Callable], **kwargs: Any) -> None:
+    def run(self):
+        self.exc = None
+        if self._exceptions is not None:
+            try:
+                threading.Thread.run(self)
+            except self._exceptions as e:
+                self.exc = e
+        else:
+            threading.Thread.run(self)
+
+    def join(self, timeout: Union[float, None] = None):
+        threading.Thread.join(self, timeout)
+        if self.exc:
+            raise self.exc
+
+
+class ThreadingPolicy(Policy):
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+        self.threads = []
+
+    def prepare_callbacks(
+        self, callbacks: List[CallbackFunction], **kwargs: Any
+    ) -> None:
         """Prepares the threads but does not start them."""
         self.threads = [
-            threading.Thread(
+            ExceptionThread(
                 target=callback,
                 kwargs=get_callback_kwargs(callback=callback, kwargs=kwargs),
+                exceptions=self.exceptions,
             )
             for callback in callbacks
         ]
 
-    def start(self) -> None:
+    def _start(self) -> None:
         """Starts all the prepared threads."""
-        threading.setprofile(self.tracer)
         for thread in self.threads:
             thread.start()
 
-    def join(self) -> None:
+    def _join(self) -> None:
         """Waits for all the threads to complete."""
         for thread in self.threads:
             thread.join()
-        threading.setprofile(None)
+
+    def _set_trace_profiler(self, tracer:Union[ProfileFunction, None]) -> None:
+        threading.setprofile(tracer)
 
 
 import multiprocessing
 
 
 class MultiprocessingPolicy(Policy):
-    def __init__(self):
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
         self.processes = []
 
-    def prepare_callbacks(self, callbacks: List[Callable], **kwargs: Any) -> None:
+    def prepare_callbacks(
+        self, callbacks: List[CallbackFunction], **kwargs: Any
+    ) -> None:
         """Prepares the processes but does not start them."""
         self.processes = [
             multiprocessing.Process(
@@ -120,12 +192,12 @@ class MultiprocessingPolicy(Policy):
             for callback in callbacks
         ]
 
-    def start(self) -> None:
+    def _start(self) -> None:
         """Starts all the prepared processes."""
         for process in self.processes:
             process.start()
 
-    def join(self) -> None:
+    def _join(self) -> None:
         """Waits for all the processes to complete."""
         for process in self.processes:
             process.join()
